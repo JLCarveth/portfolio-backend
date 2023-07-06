1e5257aad73fcfac1f938664dfcf3b9c177ac336
John Carveth <jlcarveth@gmail.com>
2023-07-06 08:47:18 -0400

    Formatting

--- controllers/MessageController.js
+++ controllers/MessageController.js
@@ -1,45 +1,51 @@
 /**
  * Mail Service
  */
-const MailDispatcher = new (require('../services/mail'))();
+const MailDispatcher = new (require("../services/mail"))();
 
 /**
  * Handles the business logic for Message domain objects
  */
 class MessageController {
-    /**
-     * Create a new MessageController, injecting the required repository
-     * @param {MessageRepository} MessageRepository 
-     */
-    constructor (MessageRepository) {
-        this.repository = MessageRepository;
-    }
+  /**
+   * Create a new MessageController, injecting the required repository
+   * @param {MessageRepository} MessageRepository
+   */
+  constructor(MessageRepository) {
+    this.repository = MessageRepository;
+  }
 
-    /**
-     * 
-     * @param {*} name 
-     * @param {*} email 
-     * @param {*} message 
-     */
-    onMessage(name, email, message) {
-        return new Promise((resolve, reject) => {
-            this.repository.createMessage(name, email, message).then(
-                MailDispatcher.sendMessage({
-                    'from': email,
-                    'to': process.env.emailAddress,
-                    'subject': 'You\'ve got mail!',
-                    'text': "Sender: " + email
-                        + "\nName: " + name
-                        + "\nMessage: " + message
-                }).catch((error) => {
-                    // Error sending the message
-                    reject("Error dispatching email");
-                }).then(resolve())
-            ).catch((error) => {
-                // Error creating the message in the database
-                reject("Error inserting to the database");
-            });
+  /**
+   *
+   * @param {*} name
+   * @param {*} email
+   * @param {*} message
+   */
+  onMessage(name, email, message) {
+    console.log(`MessageController.onMessage(${name}, ${email}, ${message})`);
+    return new Promise((resolve, reject) => {
+      this.repository
+        .createMessage(name, email, message)
+        .then(
+          MailDispatcher.sendMessage({
+            from: email,
+            to: process.env.EMAIL,
+            subject: "You've got mail!",
+            text:
+              "Sender: " + email + "\nName: " + name + "\nMessage: " + message,
+          })
+            .catch((error) => {
+              // Error sending the message
+              reject("Error dispatching email");
+            })
+            .then(resolve())
+        )
+        .catch((error) => {
+          // Error creating the message in the database
+          reject("Error inserting to the database");
         });
-    }
+    });
+  }
 }
 module.exports = MessageController;
+
